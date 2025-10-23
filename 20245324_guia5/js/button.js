79ae1e0e6da4e2d4f2fec7d8358df4379c39c9d7
Melisa Eugenia Rivas Linares <20245324@esen.edu.sv>
2025-10-22 18:59:25 -0600

    Guia 4 de Web, sin portafolio academico

---
+++ 20245324_guia5/js/button.js
@@ -0,0 +1,26 @@
+function aviso(){
+    alert('Bienvenido al mundo de JavaScript')
+}
+
+function confirmacion(){
+    // Puede guardar true o false
+
+    let confirmacion = confirm('¿Desea salir de la sesion?')
+
+    alert(`Valor seleccionado ${confirmacion}`)
+}
+
+function capturarDatos(){
+    let nombre = prompt('¿Cual es su nombre?')
+
+    let edad = prompt('¿Cual es su edad?', 0)
+
+    alert(`Su nombre es ${nombre} y su edad ${edad}`)
+}
+
+function dibujarParrafo() {
+    let parrafo = prompt('Escriba la informacion que desea visualiar en el parrafo')
+
+    const p = document.querySelector('#idParrafo')
+    p.innerHTML = parrafo
+}
